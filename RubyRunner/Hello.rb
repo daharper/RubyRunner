91df7d95dc27d01cf8650ad0cfd61c6126ac93f9
daharper <daharper@hotmail.com>
2025-09-17 02:36:20 +0100

    Add project files.

---
+++ RubyRunner/Hello.rb
@@ -0,0 +1,8 @@
+﻿class Greeter
+  def greet
+	puts 'Hello World!'
+  end
+end
+
+greeter = Greeter.new
+greeter.greet
---
+++ RubyRunner/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace RubyRunner;
+
+/// <summary>
+/// Simple C# program to execute Ruby scripts, compatible with Ruby 1.9.2, using IronRuby.
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// Loads and executes a Ruby script specified by the first command-line argument.
+    /// If no argument is provided, it defaults to executing the script "Hello.rb".
+    /// </summary>
+    internal static void Main(string[] args)
+    { 
+        var path = args.Length > 0 ? args[0] : "Hello.rb";
+        var text = File.Exists(path) ? File.ReadAllText(path) : "";
+        var code = text.Trim();
+        
+        try
+        {
+            var runtime = IronRuby.Ruby.CreateRuntime();
+            var engine = runtime.GetEngine("ruby");
+            var scope = engine.CreateScope();
+
+            engine.Execute(code, scope);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
+
